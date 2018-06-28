099ae74a8270f2ac42717595a185824a575f8c18
Edsel Caprice <caprice@mail.usf.edu>
2018-06-28 00:39:26 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,27 @@
 class Song
-attr_accessor :name, :artist, :genre
+attr_accessor :name
+attr_reader :artist, :genres
 @@counter = 0
+@@artists = []
+@@genres = []
   def initialize(name)
     @name = name
     @@counter += 1
+    @@artist << name
+  end
+
+  def count
+    @@counter
+  end
+
+  def artists
+    @@artists << artists
+    @artist = artists
+  end
+
+  def genres=(genres)
+    @@genres << genres
+    @genres = genres
     
   end
-  
-
 end
